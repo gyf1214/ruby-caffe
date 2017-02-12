27594397bce9479a90a9a5207bb7f82294fd9916
gyf1214 <gyf1214@gmail.com>
2017-02-12 10:59:46 +0800

    nothing

--- ext/caffe/caffe.cc
+++ ext/caffe/caffe.cc
@@ -1,10 +1,11 @@
-#include <rice/Class.hpp>
 #include <caffe/caffe.hpp>
 #include "blob.hpp"
 #include "net.hpp"
 
 extern "C"
 void Init_caffe() {
+    ::google::InitGoogleLogging("ruby-caffe");
+
     Init_blob();
     Init_net();
 }
