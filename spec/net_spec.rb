16181fa6590f7e44c361ae065f63c85946aa3ae5
gyf1214 <gyf1214@gmail.com>
2017-02-12 11:14:25 +0800

    add some net spec

---
+++ spec/net_spec.rb
@@ -0,0 +1,23 @@
+require 'require_all'
+require_rel '../lib/caffe.rb'
+
+RSpec.describe Caffe::Net do
+  before :example do
+    path = File.expand_path '../test.prototxt', __FILE__
+    @net = Caffe::Net.new path, Caffe::TEST
+  end
+
+  it 'can gets the input' do
+    expect(@net.inputs).to be_an(Array)
+    expect(@net.inputs.size).to eq(1)
+    input = @net.inputs[0]
+    expect(input).to be_a(Caffe::Blob)
+    expect(input.shape).to eq([1, 10, 1, 1])
+  end
+
+  it 'can get blob by name' do
+    blob = @net.blob('ip')
+    expect(blob).to be_a(Caffe::Blob)
+    expect(blob.shape).to eq([1, 100, 1, 1])
+  end
+end
