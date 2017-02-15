b228007950b5b3573ee582d7d47bf1314be7dc34
gyf1214 <gyf1214@gmail.com>
2017-02-15 20:34:30 +0800

    add test net model

---
+++ spec/net/gen_data.rb
@@ -0,0 +1,28 @@
+require 'require_all'
+require 'lmdb'
+require_rel '../../lib/caffe.rb'
+
+def gen
+  data = Array.new 32 do
+    Random.rand 2
+  end
+
+  num = data.inject(0) do |i, x|
+    i = 2 * i + x
+  end
+
+  label = ((num * num) % 1024 > 1024 / 2) ? 1 : 0
+
+  datum = Caffe::Datum.new channels: 32, height: 1, width: 1, label: label
+  datum.data = data.pack 'C*'
+
+  datum.serialize_to_string
+end
+
+env = LMDB.new File.expand_path('../test_data', __FILE__)
+env.mapsize = 1 * 1024 * 1024 * 1024
+db = env.database
+
+25600.times do |i|
+  db['%08d' % i] = gen
+end
